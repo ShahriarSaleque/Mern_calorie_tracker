0836d10c4b70f35f04ca4cfa40a96b36eac6f640
ShahriarSaleque <salequeshahriar@gmail.com>
2019-11-13 15:08:28 +0600

    user entry and hash password

---
+++ routes/user.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+
+//Bring in User Model
+const User = require("../models/User");
+
+//@route POST
+//desc register user and give jwt
+//method PUBLIC
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Valid Email address is required").isEmail(),
+    check("phone", "Phone number must be greater than 8 numbers").isLength({
+      min: 9
+    }),
+    check("password", "Password must be atleast 4 characters").isLength({
+      min: 4
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      //Destructure data from the req body
+      const { name, email, phone, password } = req.body;
+      try {
+        let user;
+        //check to see if the user exists
+        user = await User.findOne({ email });
+        if (user) {
+          return res.status(400).json({ msg: "User already exists " });
+        } else {
+          //Create new user and insert to DB
+          user = new User({
+            name,
+            email,
+            phone,
+            password
+          });
+
+          //Hash the password
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(password, salt);
+
+          await user.save();
+          return res.json("Success");
+
+          //Send back a Json Web Token
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 5000;
 //import DB connection method
 require("./config/db");
 
+//Middleware
+app.use(express.json());
+
+//Route re-direction
+app.use("/api/users", require("./routes/user"));
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
